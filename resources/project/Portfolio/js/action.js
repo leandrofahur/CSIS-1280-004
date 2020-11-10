f9a85ce9b90933eb1707fb35ffe1a1f79035abbb
leandrofahur <leandrofahur@gmail.com>
2020-11-09 17:41:41 -0800

    index.html - type effect

--- resources/project/Portfolio/js/action.js
+++ resources/project/Portfolio/js/action.js
@@ -1,5 +1,33 @@
 const blink = document.getElementById("blink");
+const typeEffect = document.getElementById("typeEffect");
+
+let isWriting = true;
+let i = 0;
+let txt = "the most precious resource you have...";
+// let txt = "test...";
+
+const Effect = () => {
+  if (isWriting) {
+    typeEffect.innerHTML += txt.charAt(i);
+    i++;
+    console.log(i);
+    setTimeout(Effect, 100);
+    if (i == txt.length) {
+      isWriting = false;
+    }
+  } else {
+    // typeEffect.innerHTML = typeEffect.innerHTML.replace(txt.charAt(i), "");
+    typeEffect.innerHTML = typeEffect.innerHTML.slice(0, -1);
+    i--;
+    setTimeout(Effect, 200);
+    if (i == 0) {
+      isWriting = true;
+    }
+  }
+};
 
 setInterval(() => {
   blink.hidden = !blink.hidden;
 }, 500);
+
+Effect();
